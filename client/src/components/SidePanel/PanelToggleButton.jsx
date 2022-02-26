4294001c6ec0d85dff198021cfb659fc35d1aa5e
Terra Smith <t_m@linuxmail.org>
2022-02-26 12:17:48 -0800

    created nav and side panel components

---
+++ client/src/components/SidePanel/PanelToggleButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PanelToggleButton.css'
+
+const panelToggleButton = props => {
+    return (
+        <button className="toggle-button" onClick={props.click}>
+            <div className="toggle-button_line" />
+            <div className="toggle-button_line" />
+            <div className="toggle-button_line" />
+        </button>
+    )
+
+};
+
+export default panelToggleButton;
