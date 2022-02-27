c9af24f65a2d7ab472c4505049b2c449a7d9ef93
Terra Smith <t_m@linuxmail.org>
2022-02-26 19:02:36 -0800

    modified toolbar.jsx -- added resume link

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -18,7 +18,7 @@ const toolbar = props => {
                         <li><a href="/about">About</a></li>
                         <li><a href="/projects">Projects</a></li>
                         <li><a href="/skills">Skills</a></li>
-                        <li><a href="/links">Links</a></li>
+                        <li><a href="/resume">Resume</a></li>
                     </ul>
                 </div>
             </nav>
