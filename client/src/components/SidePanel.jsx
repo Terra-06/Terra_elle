411cbf434ee43ef521deb13f1f539de67014de34
Terra Smith <t_m@linuxmail.org>
2022-02-26 19:02:11 -0800

    modified sidepanle.jsx -- added resume link

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -12,10 +12,10 @@ const sidePanel = props => {
         <nav className={panelClasses}>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">About</a></li>
-                <li><a href="/">Projects</a></li>
-                <li><a href="/">Skills</a></li>
-                <li><a href="/">Links</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/skills">Skills</a></li>
+                <li><a href="/resume">Resume</a></li>
             </ul>
         </nav>
     )
