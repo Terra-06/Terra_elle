f24b0ec3adc6c0d683d89212becc52ba03890380
Terra Smith <t_m@linuxmail.org>
2022-02-28 12:15:00 -0800

    modified toolbar.jsx -- updated logo, fonts and links

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles/Toolbar.css'
 import PanelToggleButton from './PanelToggleButton'
+import headphones from '../imgs/headphones.png'
+import { Link } from 'react-router-dom'
 
 const toolbar = props => {
     return (
@@ -9,15 +11,15 @@ const toolbar = props => {
                 <div className="toolbar_toggle-button">
                     <PanelToggleButton click={props.handlePanelToggleClick} />
                 </div>
-                <div className="toolbar_logo"><a href="/">LOGO</a>
+                <div className="toolbar_logo"><Link to='/'><img src={headphones} width="150px" height="90px" className='logo' alt='logo' /> </Link>
                 </div>
                 <div className="spacer" />
                 <div className="toolbar_nav-items">
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/projects">Projects</a></li>
-                        <li><a href="/skills">Skills</a></li>
+                        <li><a href="/">Home *</a></li>
+                        <li><a href="/about">About *</a></li>
+                        <li><a href="/projects">Projects *</a></li>
+                        <li><a href="/skills">Skills *</a></li>
                         <li><a href="/resume">Resume</a></li>
                     </ul>
                 </div>
