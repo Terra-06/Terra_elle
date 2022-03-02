9045e5194cf261a09797cc0a0f09b804713ef5c6
Terra Smith <t_m@linuxmail.org>
2022-03-01 16:42:42 -0800

    modified sidepanel -- adjusted responsive

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -13,9 +13,10 @@ const sidePanel = props => {
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
+                <li><a href="/resume">Resume</a></li>
                 <li><a href="/projects">Projects</a></li>
                 <li><a href="/skills">Skills</a></li>
-                <li><a href="/resume">Resume</a></li>
+
             </ul>
         </nav>
     )
