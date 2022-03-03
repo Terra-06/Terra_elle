1bb7a790f04ceffd5c723a8792a048a62f8dc692
Terra Smith <t_m@linuxmail.org>
2022-03-03 15:16:04 -0800

    clean-up code

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -25,13 +25,13 @@ export default function About() {
                     <div className='about_text-contain'>
                         <img src={Tprofile} alt='Terra profile image' className='about_image'></img>
                         <p>
-                            Terra is a DJ/Producer who recently graduated from {''}<a href='https://generalassemb.ly/'>General Assembly</a>, A full-time, complete immersive software engineering program which focused on relevant industry standard technologies including: Javascript, Python, HTML, CSS, Django, Vue.js, React, Node, GitHub, PostgreSQL, Express and MongoDB.
+                            Terra is a DJ/Producer who recently graduated from {''}<a target="_blank" href='https://generalassemb.ly/'>General Assembly</a>, A full-time, complete immersive software engineering program which focused on relevant industry standard technologies including: Javascript, Python, HTML, CSS, Django, Vue.js, React, Node, GitHub, PostgreSQL, Express and MongoDB.
                         </p>
                         <p>When Terra is not mixing and producing music, she can be found honing her coding skills on various group and independent projects.</p>
                         <p>Several of her most notable projects include:<br></br>
-                            {''} <a href='https://musi-guess.surge.sh/'>Musi'Guess</a> a 'guess-the-word' game featuring over 50 musical terms.<br></br>
-                            {''}<a href='https://tajjmusichall.herokuapp.com/'>Tajj Mu'Hall</a> a group project which features real-time events and social activities.<br></br>
-                            {''} <a href='https://anthem-music-app.herokuapp.com/'>Anthem Music</a> Terra's capstone project, complete with user authentication and 3rd party music API</p>
+                            {''} <a target="_blank" href='https://musi-guess.surge.sh/'>Musi'Guess</a> a 'guess-the-word' game featuring over 50 musical terms.<br></br>
+                            {''}<a target="_blank" href='https://tajjmusichall.herokuapp.com/'>Tajj Mu'Hall</a> a group project which features real-time events and social activities.<br></br>
+                            {''} <a target="_blank" href='https://anthem-music-app.herokuapp.com/'>Anthem Music</a> Terra's capstone project, complete with user authentication and 3rd party music API</p>
                     </div>
                 </div>
             </div>
